849e5a59f94dd9cb57a72587f4c1cba7aec191d8
taoguangc <taoguangc@gmail.com>
2023-10-19 09:32:54 +0800

    add decapcms intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,50 +4,9 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import alpinejs from '@astrojs/alpinejs'
 import robotsTxt from 'astro-robots-txt'
-import NetlifyCMS from 'astro-netlify-cms'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    alpinejs(),
-    robotsTxt(),
-    NetlifyCMS({
-      config: {
-        backend: {
-          name: 'git-gateway',
-          branch: 'main',
-        },
-        media_folder: 'src/content/posts',
-        collections: [
-          {
-            name: 'posts',
-            label: 'Blog Posts',
-            folder: 'src/content/posts',
-            create: true,
-            delete: true,
-            fields: [
-              { name: 'title', widget: 'string', label: 'Post Title' },
-              { name: 'description', widget: 'string', label: 'Post Description', required: false },
-              { name: 'pubDate', widget: 'datetime', label: 'Publish Date' },
-              { name: 'heroImage', widget: 'image', label: 'Hero Image' },
-              { name: 'body', widget: 'markdown', label: 'Post Body' },
-            ],
-          },
-          {
-            name: 'menu',
-            label: '菜单',
-            file: 'src/data/menu.json',
-            fields: [
-              { name: 'title', widget: 'string', label: '标题' },
-              { name: 'link', widget: 'string', label: '链接' },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs(), robotsTxt()],
 })
